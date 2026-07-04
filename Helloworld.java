5c74d9d691feb3a5a601791e90209c162cb3261a
sams2u36 <122690584+sams2u36@users.noreply.github.com>
2026-07-04 19:29:48 +0530

    Update Helloworld.java
    Second time to check if it triggers as there was an issue in the original yaml file

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! . Changing again to check if it triggers");
+        System.out.println("Hello, World! . Changing again to check if it triggers.SecondTime");
     }
 }
