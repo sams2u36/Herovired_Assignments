06a43ed2dccf48ad6d3dd36de151b5abdbf2b548
sams2u36 <122690584+sams2u36@users.noreply.github.com>
2026-07-04 19:48:44 +0530

    Create python2.py
    Initial Commit

---
+++ python2.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+print("Python artifact demo") 
+with open("results.txt","w") as file:
+  file.write("Hello from GitHub\n")
+  file.write("This file is created in Git\n")
+  file.write(f"Genaretd Time: {datetime.now()}\n") 
+print ("result.txt file is created successfully")
